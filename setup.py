20982144c35b54acff5060fa07b1b8a8fd85cd91
Jafeer Ali <Jafeer_Ali@comcast.com>
2025-07-21 13:58:50 +0530

    pyarrow dependency added for pytest coverage

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         'pyspark==3.5.6'
     ],
     extras_require={
-        "dev": ["pytest>=5", "pytest-cov", "pandas"]
+        "dev": ["pytest>=5", "pytest-cov", "pandas", "pyarrow"]
     },
     python_requires='>=3.7',
 )
